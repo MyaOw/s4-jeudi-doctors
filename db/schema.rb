c6bd02908c72df20e1f81193892877dd659b7291
Pauline <mya@mya-ow.net>
2018-07-26 18:47:30 +0200

    exercice 4

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_26_124229) do
+ActiveRecord::Schema.define(version: 2018_07_26_155402) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "date"
@@ -18,22 +18,46 @@ ActiveRecord::Schema.define(version: 2018_07_26_124229) do
     t.datetime "updated_at", null: false
     t.integer "doctor_id"
     t.integer "patient_id"
+    t.integer "city_id"
+    t.index ["city_id"], name: "index_appointments_on_city_id"
     t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
     t.index ["patient_id"], name: "index_appointments_on_patient_id"
+  end
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "doctors", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "specialty"
     t.integer "postal_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "city_id"
+    t.index ["city_id"], name: "index_doctors_on_city_id"
+  end
+
+  create_table "doctors_specialties", id: false, force: :cascade do |t|
+    t.integer "specialty_id", null: false
+    t.integer "doctor_id", null: false
+    t.index ["doctor_id", "specialty_id"], name: "index_doctors_specialties_on_doctor_id_and_specialty_id"
+    t.index ["specialty_id", "doctor_id"], name: "index_doctors_specialties_on_specialty_id_and_doctor_id"
   end
 
   create_table "patients", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "city_id"
+    t.index ["city_id"], name: "index_patients_on_city_id"
+  end
+
+  create_table "specialties", force: :cascade do |t|
+    t.string "name_specialty"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,20 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
-10.times do
-  Doctor.create(first_name: Faker::Pokemon.name, last_name: Faker::Pokemon.location, specialty: Faker::Military.army_rank, postal_code: Faker::Address.zip)
+10.times do 
+  City.create(name: Faker::Pokemon.location)
 end
 
 10.times do
-  Patient.create(first_name: Faker::Dessert.variety, last_name: Faker::Dessert.flavor)
+  d = Doctor.create(first_name: Faker::Pokemon.name, last_name: Faker::Pokemon.location, postal_code: Faker::Address.zip, city_id: rand(1..10))
+  s = Specialty.create(name_specialty: Faker::Dessert.variety)
+  d.specialty_ids = s.id
 end
 
 10.times do
-  Appointment.create(date: Faker::Time.between(2.days.ago, Date.today, :all), doctor_id: rand(1..10), patient_id: rand(1..10))
+  Patient.create(first_name: Faker::Dessert.variety, last_name: Faker::Dessert.flavor, city_id: rand(1..10))
+end
+
+10.times do
+  Appointment.create(date: Faker::Time.between(2.days.ago, Date.today, :all), doctor_id: rand(1..10), patient_id: rand(1..10), city_id: rand(1..10))
 end
